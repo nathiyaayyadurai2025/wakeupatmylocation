3c7b1915f070ae5b1a17f67076602da2b78043f4
Nathiya Karthikeyan <nathiyaayyadurai2025@gmail.com>
2026-04-17 14:08:43 +0530

    fix: remove mapbox, fix dependencies, PWA setup

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      includeAssets: ['favicon.svg', 'apple-touch-icon.png', 'icon.svg'],
       manifest: {
         name: 'WakeMyStop | Travel Alarm',
         short_name: 'WakeMyStop',
